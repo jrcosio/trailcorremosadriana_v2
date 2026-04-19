565d2b65808385b6d38303071cb4a6c3c67a8321
J. Ramón Blanco <jr.cosio1@gmail.com>
2026-04-19 11:52:14 +0200

    Agregar pie de página a la página de recorridos

--- trailcorremosadriana_v2/pages/recorridos/recorridos.py
+++ trailcorremosadriana_v2/pages/recorridos/recorridos.py
@@ -1,5 +1,6 @@
 import reflex as rx
 from trailcorremosadriana_v2.components.barra_navegacion import barra_de_navegacion
+from trailcorremosadriana_v2.components.pie_pagina import pie_pagina
 
 
 
@@ -11,12 +12,14 @@ def recorridos() -> rx.Component:
             # Resto del contenido de la página
             rx.text("Recorridos", size="6", font_weight="bold", align="center", color="black"),
             rx.text("En Construcción", size="6", font_weight="bold", align="center", color="black"),
-  
+            rx.text("¡Próximamente podrás explorar nuestros emocionantes recorridos! Estamos trabajando arduamente para ofrecerte experiencias inolvidables. Mantente atento a nuestras actualizaciones para descubrir las aventuras que tenemos preparadas para ti.", size="5", align="center", color="black"),
+            
             
             spacing="5",
             width="100%",
             
         ),
+        pie_pagina(),
         background_color="#EBEBEB",
 
     )
