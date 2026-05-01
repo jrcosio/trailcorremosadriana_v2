37653150c163adb466086d41f4244a9d08d77a2e
Jose <jolugucos@gmail.com>
2026-05-01 10:08:31 +0200

    Agregar imágenes de perfil y actualizar la estructura de la página de recorridos

--- trailcorremosadriana_v2/pages/recorridos/recorridos.py
+++ trailcorremosadriana_v2/pages/recorridos/recorridos.py
@@ -4,15 +4,11 @@ from trailcorremosadriana_v2.components.pie_pagina import pie_pagina
 from trailcorremosadriana_v2.components.carreras import carreras
 
 
+
 def recorridos() -> rx.Component:
     return rx.box(
         barra_de_navegacion(),
-        rx.vstack(
-           
         carreras(),
-            
-
-        ),
         pie_pagina(),
         background_color="#EBEBEB",
 
